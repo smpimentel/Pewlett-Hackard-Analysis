6d686594bd27a12c09db274d92d7528a98479976
Stephen  Pimentel <Stephen1@Stephens-MacBook-Pro.local>
2022-07-30 15:42:50 -0700

    created new table and updated schema.

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -8,22 +8,22 @@ CREATE TABLE departments(
 
 CREATE TABLE employees(
 	emp_no INT NOT NULL,
-	bith_date DATE NOT NULL,
-	fist_name VARCHAR NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
 	last_name VARCHAR NOT NULL,
 	gender VARCHAR NOT NULL,
 	hire_date DATE NOT NULL,
 	PRIMARY KEY(emp_no)
 );
 
-CREATE TABLE dept_manager(
-dept_no VARCHAR(4) NOT NULL,
-	emp_no INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no, dept_no)
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE salaries(
@@ -35,4 +35,34 @@ CREATE TABLE salaries(
 	PRIMARY KEY (emp_no)
 );
 
-SELECT * FROM departments;
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+	PRIMARY KEY (emp_no, title, from_date)
+);
+
+-- Cheackng all data from table
+SELECT * FROM titles;
+
+ALTER TABLE employees
+RENAME COLUMN bith_date TO birth_date;
+
+-- Retirment eligibility
+SELECT first_name, Last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
